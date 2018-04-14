e4ab9e93c80df9ca7d44c24aebc5faa0be7f8e2d
Igor <IHateWin7@gmail.com>
2018-04-14 21:27:55 +0700

    subtCode

--- test_Kut.py
+++ test_Kut.py
@@ -4,32 +4,40 @@ import pymorphy2
 from gensim.models import Doc2Vec
 from gensim.models.doc2vec import LabeledSentence, Doc2Vec
 
-with open("canonized_clear.pickle",'rb') as file:
-    asd = pickle.load(file)
+modelLen = 4000000
+
+sentences = []
+with open("Processed_sent.txt",'r') as file:
+    i = 0
+    while (i < modelLen):
+        if (not i % 10000):
+            print("Loaded " + str(i) + " sentences")
+        sentences.append(file.readline())
+        i+=1
+print("Loaded " + str(modelLen) + " sentences")
+print("All sentences loaded. Mapping over list")
+
+sentences = list(map(lambda x: x.split(),sentences))
+print ('Data loaded')
 #%%
-print("LOADED")
-sentences = list(asd[0]['context_0'])
-sentences.extend(asd[1]['context_0'])
-sentences.extend(asd[1]['context_1'])
-sentences.extend(asd[2]['context_0'])
-sentences.extend(asd[2]['context_1'])
-sentences.extend(asd[2]['context_2'])
 
-#%%
+model = gensim.models.Word2Vec(alpha=0.025, min_alpha=0.025, min_count=1)
+print("Model created")
 
-#model = gensim.models.Word2Vec(sentences, min_count=5, window = 5)
-#model.wv.most_similar_cosmul(positive=['город'])
-
-model = gensim.models.Word2Vec(alpha=0.025, min_alpha=0.025)
-print("GENISMED")
 model.build_vocab(sentences)
-print("VOCAB")
+
+model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
+print("Vocab built")
 for epoch in range(10):
+    print ("Training, iteration " + str(epoch))
     model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
     model.alpha -= 0.002
     model.min_alpha = model.alpha
 print("TRAINED")
+
 #%%
-model = gensim.models.KeyedVectors.load_word2vec_format("C:\\Users\\User\\OneDrive\\MLTrack\\yandex_speaker\\ruscorpora_upos_skipgram_300_5_2018.vec", binary=False)
+with open("model_on_subtitles" + str(modelLen) + ".pickle","wb") as pcl:
+    pickle.dump(model, pcl)
+
 #%%
-print(model.wv.most_similar(positive=['любить_VERB','собака_NOUN']))
+print(model.wv.most_similar(positive=['ребенок_NOUN']))
